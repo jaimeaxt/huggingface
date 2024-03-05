dba641ec6b6f544bde71d5fd05d800535bcca906
Jaime Axt <jaime.axt@gmail.com>
2024-03-05 11:54:00 -0300

    V1

---
+++ test.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+os.environ["HF_TOKEN"] = os.getenv('HF_TOKEN')
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-7b")
+model = AutoModelForCausalLM.from_pretrained("google/gemma-7b", device_map="auto")
+
+input_text = "¿Cómo es Chile?"
+input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
+
+outputs = model.generate(**input_ids)
+
+#MODIFICACIÓN
+
+for x in outputs:
+    print(tokenizer.decode(x))
